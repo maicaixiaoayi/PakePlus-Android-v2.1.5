a3b0cc1c04d154bbe9345db4e9482e808f908cbf
maicaixiaoayi <132263794+maicaixiaoayi@users.noreply.github.com>
2025-12-30 16:40:08 +0800

    update src file from pakeplus

---
+++ scripts/www/constants.ts
@@ -0,0 +1,9 @@
+import { Room } from './types.ts';
+
+export const ROOMS: Room[] = [
+  { id: '1', name: '最幸福', capacity: '10-12人' },
+  { id: '2', name: '最快乐', capacity: '8-10人' },
+  { id: '3', name: '最开心', capacity: '12-16人' }
+];
+
+export const STORAGE_KEY = 'mobile_reservations_v2';
